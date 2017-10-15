dce02d7a8845fe7f6e4d419767dd30d28714111c
Mazk <ma-zk@live.com>
2017-10-16 01:47:52 +0800

    init

---
+++ demo.js
@@ -0,0 +1,10 @@
+var ap = new APlayer({
+    
+    music: {                                                           // Required, music info, see: ###With playlist
+        title: 'Determination Symphony',                                          // Required, music title
+        author: 'Roselia',                          // Required, music author
+        url: 'Roselia-Determination Symphony.mp3',  // Required, music url
+        pic: '076_symphony_jacket.png',  // Optional, music picture
+      
+    }
+});
