67f542ff69520cad4a9badf721a4285c829c2929
Mazk <ma-zk@live.com>
2017-10-20 18:43:46 +0800

    ロメオ

--- demo.js
+++ demo.js
@@ -1,10 +1,10 @@
 var ap = new APlayer({
     
     music: {                                                           // Required, music info, see: ###With playlist
-        title: 'Determination Symphony',                                          // Required, music title
-        author: 'Roselia',                          // Required, music author
-        url: 'Roselia-Determination Symphony.mp3',  // Required, music url
-        pic: '076_symphony_jacket.png',  // Optional, music picture
+        title: 'ロメオ',                                          // Required, music title
+        author: 'ハロー、ハッピーワールド！',                          // Required, music author
+        url: 'romeo.mp3',  // Required, music url
+        pic: 'romeo.png',  // Optional, music picture
       
     }
 });
