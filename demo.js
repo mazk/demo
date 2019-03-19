a8a777a5f9a8a78a5256cd5a9ec458223216ba68
Mazk <ma-zk@live.com>
2019-03-20 04:15:39 +0800

    m

--- demo.js
+++ demo.js
@@ -3,11 +3,7 @@
     music: {                                                           // Required, music info, see: ###With playlist
         title: 'わがまま',                                          // Required, music title
         author: '瀧川ありさ',                          // Required, music author
-<<<<<<< HEAD
         url: '1.mp3',  // Required, music url
-=======
-        url: 'Wagamama.mp3',  // Required, music url
->>>>>>> origin/gh-pages
         pic: 'Cover.jpg',  // Optional, music picture
       
     }
