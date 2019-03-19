56be2ef96e4383e87a69dde27840a7e20aeb4de3
Mazk <ma-zk@live.com>
2019-03-20 04:06:35 +0800

    Wagamama

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@ var ap = new APlayer({
     music: {                                                           // Required, music info, see: ###With playlist
         title: 'わがまま',                                          // Required, music title
         author: '瀧川ありさ',                          // Required, music author
-        url: 'wagamama.mp3',  // Required, music url
+        url: 'Wagamama.mp3',  // Required, music url
         pic: 'Cover.jpg',  // Optional, music picture
       
     }
