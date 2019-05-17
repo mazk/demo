b6e62f4cfbc943d0d05ec2947686e0dd7cb3c0ae
Mazk <ma-zk@live.com>
2019-05-18 01:25:32 +0800

    "Kiss Me"

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
 ﻿var ap = new APlayer({
     
     music: {                                                           // Required, music info, see: ###With playlist
-        title: 'センチメンタルクライシス',                                          // Required, music title
-        author: 'halca',                          // Required, music author
+        title: 'Kiss Me',                                          // Required, music title
+        author: 'Nai Br.XX&Celeina Ann',                          // Required, music author
         url: '1.mp3',  // Required, music url
         pic: 'Cover.jpg',  // Optional, music picture
       
