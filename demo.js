4a43e6536d0898de6ac87300e8961e9d0939b2ac
Mazk <ma-zk@live.com>
2019-09-25 21:36:01 +0800

    takagi3 CSC

--- demo.js
+++ demo.js
@@ -1,10 +1,64 @@
-﻿var ap = new APlayer({
-    
-    music: {                                                           // Required, music info, see: ###With playlist
-        title: 'Kiss Me',                                          // Required, music title
-        author: 'Nai Br.XX&Celeina Ann',                          // Required, music author
-        url: '1.mp3',  // Required, music url
-        pic: 'Cover.jpg',  // Optional, music picture
-      
-    }
+﻿const ap = new APlayer({
+    container: document.getElementById('player'),
+    listFolded: false,
+    listMaxHeight: 900,
+    volume: 0.5,
+    audio: [
+        {
+            name: '奏(かなで)',
+            artist: '高木さん(CV:高橋李依)',
+            url: '1.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: '粉雪',
+            artist: '高木さん(CV:高橋李依)',
+            url: '2.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: 'キセキ',
+            artist: '高木さん(CV:高橋李依)',
+            url: '3.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: 'ありがとう',
+            artist: '高木さん(CV:高橋李依)',
+            url: '4.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: 'STARS',
+            artist: '高木さん(CV:高橋李依)',
+            url: '5.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: 'あなたに',
+            artist: '高木さん(CV:高橋李依)',
+            url: '6.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: '言わないけどね。',
+            artist: '高木さん(CV:高橋李依)',
+            url: '7.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        },
+        {
+            name: 'やさしい気持ち',
+            artist: '高木さん(CV:高橋李依)',
+            url: '8.mp3',
+            cover: 'cover.jpg',
+            theme: '#F4A7B9'
+        }
+    ]
 });
