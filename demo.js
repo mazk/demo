446e714039319f2bf47c5ee3db9e2fb929cb634b
Mazk <ma-zk@live.com>
2019-09-25 22:02:26 +0800

    fix cover

--- demo.js
+++ demo.js
@@ -8,56 +8,56 @@
             name: '奏(かなで)',
             artist: '高木さん(CV:高橋李依)',
             url: '1.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: '粉雪',
             artist: '高木さん(CV:高橋李依)',
             url: '2.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: 'キセキ',
             artist: '高木さん(CV:高橋李依)',
             url: '3.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: 'ありがとう',
             artist: '高木さん(CV:高橋李依)',
             url: '4.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: 'STARS',
             artist: '高木さん(CV:高橋李依)',
             url: '5.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: 'あなたに',
             artist: '高木さん(CV:高橋李依)',
             url: '6.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: '言わないけどね。',
             artist: '高木さん(CV:高橋李依)',
             url: '7.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         },
         {
             name: 'やさしい気持ち',
             artist: '高木さん(CV:高橋李依)',
             url: '8.mp3',
-            cover: 'cover.jpg',
+            cover: 'Cover.jpg',
             theme: '#F4A7B9'
         }
     ]
