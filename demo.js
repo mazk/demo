b92e77313bcdf291f0fd80fb858b1b9b96018386
Mazk <ma-zk@live.com>
2020-10-09 00:05:51 +0800

    Revert "Revert "update""
    This reverts commit e2779e8692d0a4e5cc3d559e53fd798f03792265, reversing
    changes made to 9c123dbbf192164e8dbef39817aa3cdab3812294.

--- demo.js
+++ demo.js
@@ -2,61 +2,12 @@
     container: document.getElementById('player'),
     listFolded: false,
     listMaxHeight: 900,
-    volume: 0.5,
+    volume: 0.4,
     audio: [
         {
-            name: '奏(かなで)',
-            artist: '高木さん(CV:高橋李依)',
-            url: '1.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: '粉雪',
-            artist: '高木さん(CV:高橋李依)',
-            url: '2.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: 'キセキ',
-            artist: '高木さん(CV:高橋李依)',
-            url: '3.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: 'ありがとう',
-            artist: '高木さん(CV:高橋李依)',
-            url: '4.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: 'STARS',
-            artist: '高木さん(CV:高橋李依)',
-            url: '5.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: 'あなたに',
-            artist: '高木さん(CV:高橋李依)',
-            url: '6.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: '言わないけどね。',
-            artist: '高木さん(CV:高橋李依)',
-            url: '7.mp3',
-            cover: 'cover.jpg',
-            theme: '#F4A7B9'
-        },
-        {
-            name: 'やさしい気持ち',
-            artist: '高木さん(CV:高橋李依)',
-            url: '8.mp3',
+            name: '無敵級*ビリーバー',
+            artist: '中須かすみ(CV.相良茉優)',
+            url: '10082346.mp3',
             cover: 'cover.jpg',
             theme: '#F4A7B9'
         }
